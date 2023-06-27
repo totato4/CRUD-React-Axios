f70615eb504b92a05a4bc972f4e63ad2769480dd
totato4 <v4.97@list.ru>
2023-06-27 08:15:08 +0300

    add-footer-and-header

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { selectTodos } from "./components/RTK/todoSlice/Selectors";
 import { useAppSelector, useAppDispatch } from "./components/RTK/store";
 import { Itodo } from "./components/RTK/todoSlice/types";
 import { fetchTodo } from "./components/RTK/todoSlice/todoSlice";
+import Footer from "./components/Footer/Footer";
 
 type todosProps = {
   completed: boolean;
@@ -16,20 +17,7 @@ type todosProps = {
 };
 
 const App: React.FC = () => {
-  const [todos, setTodos] = React.useState<todosProps[] | false>(false);
-
-  React.useEffect(() => {
-    axios
-      .get("https://jsonplaceholder.typicode.com/todos?_limit=10")
-      .then((response) => {
-        setTodos(response.data);
-      });
-  }, []);
-
   const [inputValue, setInputValue] = React.useState<string>("");
-  const [createLoading, setCreateLoading] = React.useState<boolean>(false);
-  const [post, setPost] = React.useState<boolean>(false);
-  const createPost = () => setPost(!post);
 
   // FETCH TODO
   const dispatch = useAppDispatch();
@@ -40,29 +28,34 @@ const App: React.FC = () => {
   React.useEffect(() => {
     dispatch(fetchTodo());
   }, []);
-  React.useEffect(() => {}, [myTodo]);
 
   return (
-    <div className="App">
-      <div className="todos">
-        <h1>FETCH todo</h1>
-        <Input inputValue={inputValue} setInputValue={setInputValue}>
-          <Button text={inputValue} />
-        </Input>
-        <div role="items" className="todos-wrapper">
-          {status == "error" ? (
-            <div
-              onClick={() => {
-                fetchTodo();
-              }}
-            >
-              Попробовать снова
-            </div>
-          ) : (
-            myTodo.map((obj, i) => <Todos key={i} {...obj} />)
-          )}
+    <div className="wrapper">
+      <header className="header">header</header>
+      <main className="main">
+        <div className="todos">
+          <h1>FETCH todo</h1>
+          <Input inputValue={inputValue} setInputValue={setInputValue}>
+            <Button text={inputValue} />
+          </Input>
+          <div role="items" className="todos-wrapper">
+            {status == "error" ? (
+              <div
+                onClick={() => {
+                  fetchTodo();
+                }}
+              >
+                Попробовать снова
+              </div>
+            ) : (
+              myTodo.map((obj, i) => <Todos key={i} {...obj} />)
+            )}
+          </div>
         </div>
-      </div>
+      </main>
+      <footer className="footer">
+        <Footer />
+      </footer>
     </div>
   );
 };
